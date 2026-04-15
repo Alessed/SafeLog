cf5e47ca3176b68e70e62b2acaf291c2c681cc13
Alessed <alexlara0956@gmail.com>
2026-04-15 12:57:03 -0600

    Ajuste para creación automática de superuser

--- build.sh
+++ build.sh
@@ -6,3 +6,8 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Crear superusuario automáticamente si no existe
+if [ "$CREATE_SUPERUSER" ]; then
+  python manage.py createsuperuser --no-input || true
+fi
